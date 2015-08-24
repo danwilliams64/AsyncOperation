2ba9cb9dcd51bb47c2694bb64bddee8925285bed
Daniel Williams <dwilliams@brilliantbasics.com>
2015-08-24 07:49:03 +0100

    Initial Commit

---
+++ AsyncOperation/AsyncOperation.h
@@ -0,0 +1,19 @@
+//
+//  AsyncOperation.h
+//  AsyncOperation
+//
+//  Created by Daniel Williams on 24/08/2015.
+//  Copyright © 2015 Dan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncOperation.
+FOUNDATION_EXPORT double AsyncOperationVersionNumber;
+
+//! Project version string for AsyncOperation.
+FOUNDATION_EXPORT const unsigned char AsyncOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncOperation/PublicHeader.h>
+
+
