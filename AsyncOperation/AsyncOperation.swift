9a4337ff0605e5d3eabf0de349e2e361cb7a221f
Dan Williams <dan@danwilliams.co>
2015-11-20 11:33:48 +0000

    Add License. Remove call to completion block on finish execution.

--- AsyncOperation/AsyncOperation.swift
+++ AsyncOperation/AsyncOperation.swift
@@ -2,22 +2,38 @@
 //  AsyncOperation.swift
 //  AsyncOperation
 //
-//  Created by Daniel Williams on 24/08/2015.
-//  Copyright © 2015 Dan Williams. All rights reserved.
+//  Copyright (c) 2015, Dan Williams
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import Foundation
 
 public class AsyncOperation: NSOperation {
     
     // MARK: - Properties
-    public override var executing: Bool { get { return internalExecuting }}
-    public override var finished: Bool { get { return internalFinshed }}
-    public override var asynchronous: Bool { get { return true }}
+    public override var executing: Bool { return internalExecuting }
+    public override var finished: Bool { return internalFinshed }
+    public override var asynchronous: Bool { return true }
     
     internal var internalExecuting = false {
         willSet {
-           willChangeValueForKey("isExecuting")
+            willChangeValueForKey("isExecuting")
         }
         didSet {
             didChangeValueForKey("isExecuting")
@@ -48,11 +64,10 @@ public class AsyncOperation: NSOperation {
     public func execute() { fatalError("`execute` method not overridden by superclass.") }
     
     /**
-    Called from within the `execute` function when the operation has completed.
-    */
+     Called from within the `execute` function when the operation has completed.
+     */
     public func finish() {
         internalExecuting = false
         internalFinshed = true
-        self.completionBlock?()
     }
 }
